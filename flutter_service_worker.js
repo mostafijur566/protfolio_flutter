f167b82dc3735594f732fcc2099c4cd9fa9d9ca4
Mostafijur Rahman <66210185+mostafijur566@users.noreply.github.com>
2023-05-02 04:12:37 +0600

    Deploy to GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -38,8 +38,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "616c33aab0f23ca5d42278c7dd33be6d",
-"/": "616c33aab0f23ca5d42278c7dd33be6d",
+"index.html": "5784c5a2e96286b656a7b9eca7341c92",
+"/": "5784c5a2e96286b656a7b9eca7341c92",
 "main.dart.js": "e64116645ff448527047f15e29d87f0d",
 "manifest.json": "5534f376b0ca11f23a7e8ea62d6157a1",
 "version.json": "984181b4c5c078a543d133d704860952"
